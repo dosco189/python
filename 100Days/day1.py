c1fb556ccd68896ff893420d99094a7d15b58469
Arth Gupta <arthgupta@MacBook-Pro.local>
2023-04-04 18:38:34 -0400

    python files

---
+++ 100Days/day1.py
@@ -0,0 +1,10 @@
+print("Welcome to the Band Name Generator!")
+
+name = input("What is your name? ")
+city = input("What is the name of the city you grew up in? ")
+
+pet = input("What is the name of your pet? ")
+
+band_name = name + " " + city + " " + pet
+
+print("Your band name could be " + band_name)
